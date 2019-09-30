caf3d803ac127c7cd0aa4ea99ce91d0e6daf985b
Jongmin Lee <starjongmin@gmail.com>
2019-09-30 08:52:19 +0000

    update result.py

--- src/result.py
+++ src/result.py
@@ -8,7 +8,8 @@ from stable_baselines import SAC
 from stable_baselines import TRPO
 from stable_baselines.common import set_global_seeds
 from stable_baselines.common.vec_env import SubprocVecEnv
-from tqdm import tqdm
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 
 def make_vectorized_env(env_name, n_envs=multiprocessing.cpu_count()):
@@ -32,25 +33,23 @@ def make_vectorized_env(env_name, n_envs=multiprocessing.cpu_count()):
     return vec_env
 
 
-def evaluate_policy(vec_env, agent, num_episodes=30, deterministic=False):
+def evaluate_policy(vec_env, agent, num_episodes=5, deterministic=False):
     episode_rewards = []
 
-    with tqdm(total=num_episodes, desc="policy_evaluation") as pbar:
-        episode_reward = np.zeros(vec_env.num_envs)
-        obs = vec_env.reset()
-        while len(episode_rewards) < num_episodes:
-            action, _ = agent.predict(obs, deterministic=deterministic)
-            next_obs, reward, done, _ = vec_env.step(action)
+    episode_reward = np.zeros(vec_env.num_envs)
+    obs = vec_env.reset()
+    while len(episode_rewards) < num_episodes:
+        action, _ = agent.predict(obs, deterministic=deterministic)
+        next_obs, reward, done, _ = vec_env.step(action)
 
-            episode_reward = episode_reward + reward
-            if np.count_nonzero(done) > 0:
-                episode_rewards += list(episode_reward[done])
-                episode_reward[done] = 0
-                pbar.update(np.count_nonzero(done))
+        episode_reward = episode_reward + reward
+        if np.count_nonzero(done) > 0:
+            episode_rewards += list(episode_reward[done])
+            episode_reward[done] = 0
 
-            obs = next_obs
+        obs = next_obs
 
-        episode_rewards = np.array(episode_rewards)
+    episode_rewards = np.array(episode_rewards)
 
     mu = np.mean(episode_rewards)
     ste = np.std(episode_rewards) / np.sqrt(len(episode_rewards))
@@ -59,11 +58,12 @@ def evaluate_policy(vec_env, agent, num_episodes=30, deterministic=False):
 
 
 if __name__ == "__main__":
-    for seed in range(3):
-        for env_name in ['halfcheetah', 'hopper', 'ant', 'walker']:
-            vec_env = make_vectorized_env(env_name)
+    for env_name in ['hopper', 'halfcheetah', 'ant', 'walker']:
+        print('==========================')
+        vec_env = make_vectorized_env(env_name)
 
-            for algorithm in ['ppo', 'trpo', 'sac']:
+        for algorithm in ['ppo', 'trpo', 'sac']:
+            for seed in range(3):
                 model_filepath = '%s_%s_%d.pkl' % (env_name, algorithm, seed)
                 if not os.path.exists(model_filepath):
                     print("'%s' does not exists." % model_filepath)
@@ -79,4 +79,5 @@ if __name__ == "__main__":
                     raise NotImplementedError()
 
                 result_mean, result_ste = evaluate_policy(vec_env, model)
-                print("env=%s,algorithm=%s,seed=%d: %f +- %f" % (env_name, algorithm, seed, result_mean, 2 * result_ste))
+                print("env=%11s, algorithm=%4s, seed=%2d: reward_sum=%8.2f" % (env_name, algorithm, seed, result_mean))
+            print()
