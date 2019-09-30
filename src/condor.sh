b4771a93c918e8ee2fff4996f030745c5d937a24
Jongmin Lee <starjongmin@gmail.com>
2019-09-30 06:08:32 +0000

    condor.sh

--- src/condor.sh
+++ src/condor.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo $HOME
+export HOME=/home/starjongmin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco200/bin
+
 cd /mnt/nfs/gcp-condor/src
 $HOME/miniconda/envs/example-env/bin/python main.py --pid=$1
