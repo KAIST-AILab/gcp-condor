9028b3056cebd5146c457342335d478a4e63e52b
Jongmin Lee <starjongmin@gmail.com>
2019-09-30 15:51:54 +0900

    add result.py

---
+++ src/result.py
@@ -0,0 +1,83 @@
+import multiprocessing
+import time
+
+import gym
+import numpy as np
+from stable_baselines import PPO2
+from stable_baselines import SAC
+from stable_baselines import TRPO
+from stable_baselines.common import set_global_seeds
+from stable_baselines.common.vec_env import SubprocVecEnv
+from tqdm import tqdm
+
+
+def make_vectorized_env(env_name, n_envs=multiprocessing.cpu_count()):
+    env_name_map = {
+        'halfcheetah': 'HalfCheetah-v2',
+        'hopper': 'Hopper-v2',
+        'ant': 'Ant-v2',
+        'walker': 'Walker-v2'
+    }
+
+    def make_env(env_id, seed=0):
+        def _init():
+            env = gym.make(env_id)
+            env.seed(seed)
+            return env
+
+        set_global_seeds(seed)
+        return _init
+
+    vec_env = SubprocVecEnv([make_env(env_name_map[env_name], i) for i in range(n_envs)])
+    return vec_env
+
+
+def evaluate_policy(vec_env, agent, num_episodes=30, deterministic=False, render=False):
+    episode_rewards = []
+
+    with tqdm(total=num_episodes, desc="policy_evaluation") as pbar:
+        episode_reward = np.zeros(vec_env.num_envs)
+        obs = vec_env.reset()
+        while len(episode_rewards) < num_episodes:
+            action, _ = agent.predict(obs, deterministic=deterministic)
+            next_obs, reward, done, _ = vec_env.step(action)
+
+            episode_reward = episode_reward + reward
+            if np.count_nonzero(done) > 0:
+                episode_rewards += list(episode_reward[done])
+                episode_reward[done] = 0
+                pbar.update(np.count_nonzero(done))
+
+            obs = next_obs
+
+            if render:
+                vec_env.render()
+                time.sleep(0.1)
+
+        episode_rewards = np.array(episode_rewards)
+
+    mu = np.mean(episode_rewards)
+    ste = np.std(episode_rewards) / np.sqrt(len(episode_rewards))
+    print("\n%f +- %f" % (mu, ste))
+
+    return np.mean(episode_rewards)
+
+
+if __name__ == "__main__":
+    for seed in range(3):
+        for env_name in ['halfcheetah', 'hopper', 'ant', 'walker']:
+            vec_env = make_vectorized_env(env_name)
+
+            for algorithm in ['ppo', 'trpo', 'sac']:
+                model_filepath = '%s_%s_%d' % (env_name, algorithm, seed)
+                if algorithm == 'ppo':
+                    model = PPO2.load(model_filepath)
+                elif algorithm == 'trpo':
+                    model = TRPO.load(model_filepath)
+                elif algorithm == 'sac':
+                    model = SAC.load(model_filepath)
+                else:
+                    raise NotImplementedError()
+
+                result = evaluate_policy(vec_env, model)
+                print(result)
